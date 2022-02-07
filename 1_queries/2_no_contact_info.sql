197bbe9ffc16ddeb5ba9813b0b2ba9fe5677bdf1
Armin Glavovic <glavovic.armin@gmail.com>
2022-02-07 20:18:29 +0000

    added 1quries

---
+++ 1_queries/2_no_contact_info.sql
@@ -0,0 +1,3 @@
+SELECT name, id, cohort_id
+FROM students
+where email IS NULL OR phone IS NULL;
---
+++ 1_queries/5_graduate_no_github.sql
@@ -0,0 +1,3 @@
+Select name, email, phone
+from students
+WHERE end_date IS NOT NULL and github is NULL
